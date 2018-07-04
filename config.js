75261ff8517e56a8a166844ef5d22448bab80d50
emaperez <perez@thirdlove.com>
2018-07-04 13:11:16 -0300

    new changes

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    github_user: 'ci-agent-thirdlove',
+    github_password: 'Thirdlove123!',
+    user_agent: 'ESLintBot (github.com/cody-greene/eslint-bot)',
+    log_level: 'info',
+    port: '5000',
+    shutdown_idle: '3000',
+    shared_secret: '160_BIT_KEY'
+};
+
+// github_user: process.env.GH_USER,
+//   github_password: process.env.GH_TOKEN,
+//   user_agent: process.env.USER_AGENT,
+//   log_level: process.env.LOG_LEVEL,
+//   port: process.env.PORT,
+//   shutdown_idle: process.env.SHUTDOWN_SOCKET_IDLE,
+//   shared_secret: process.env.SHARED_SECRET
